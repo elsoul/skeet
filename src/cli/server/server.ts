47f92306c3b20dabb65d197d8990d34b8f8c9b86
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-04-14 10:52:41 +0200

    update kill cmd

--- src/cli/server/server.ts
+++ src/cli/server/server.ts
@@ -4,7 +4,7 @@ import { spawnSync } from 'child_process'
 export const server = async () => {
   try {
     const killEmulator =
-      "kill $(ps aux | grep firebase/emulators/cloud-firestore-emulator | awk '{print $2}')"
+      "kill $(ps aux | grep 'cloud-firestore-emulator.*host 127.0.0.1' | awk '{print $2}')"
 
     spawnSync(killEmulator, { stdio: 'ignore' })
     const shCmd = ['yarn', 'skeet']
