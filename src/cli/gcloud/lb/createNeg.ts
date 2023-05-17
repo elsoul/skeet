7ae433988f73cc3025eba0bed5f3e33d93bec25e
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-17 19:38:56 +0200

    update path matcher

--- src/cli/gcloud/lb/createNeg.ts
+++ src/cli/gcloud/lb/createNeg.ts
@@ -2,13 +2,14 @@ import { execSyncCmd } from '@/lib/execSyncCmd'
 import { getFunctionInfo } from '@/lib/getSkeetConfig'
 import { convertToKebabCase } from '@/utils/string'
 
+// This will need updates when Google Cloud Run naming changed
 export const createNeg = async (
   projectId: string,
   methodName: string,
   region: string
 ) => {
   const kebab = convertToKebabCase(methodName)
-  const cloudRunName = methodName.toLowerCase()
+  const cloudRunName = kebab.replace(/-/g, '')
   const functionInfo = await getFunctionInfo(kebab)
   const shCmd = [
     'gcloud',
