e84d3f1cfc33fb43c229612cb7041b9d7e559565
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-09-28 09:19:36 +0200

    fix gen method auth

--- src/templates/graphql/saveSolanaTransfer.ts
+++ src/templates/graphql/saveSolanaTransfer.ts
@@ -40,6 +40,5 @@ export const saveSkeetSolanaTransfer = extendType({
       },
     })
   },
-})
-  `
+})`
 }
--- src/templates/instanceTypes/genHttpMethodParams.ts
+++ src/templates/instanceTypes/genHttpMethodParams.ts
@@ -2,15 +2,14 @@ import { FUNCTIONS_PATH } from '@/lib'
 
 export const genHttpMethodParams = async (
   functionsName: string,
-  methodName: string
+  methodName: string,
 ) => {
   const firstChar = methodName.charAt(0).toUpperCase()
   const capitalizedMethodName = firstChar + methodName.slice(1)
   const filePath = `${FUNCTIONS_PATH}/${functionsName}/src/types/http/${methodName}Params.ts`
   const body = `export type ${capitalizedMethodName}Params = {
   name?: string
-}
-  `
+}`
   return {
     filePath,
     body,
