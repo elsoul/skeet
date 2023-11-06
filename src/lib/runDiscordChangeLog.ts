bac6fbfa0801525cc16f6512cd02e97e85c83c99
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-06 11:05:03 +0100

    add discord notification in Github Actions

--- src/lib/runDiscordChangeLog.ts
+++ src/lib/runDiscordChangeLog.ts
@@ -2,16 +2,25 @@ import { discordChangeLog } from '@skeet-framework/discord-utils'
 import { dotenv } from '@skeet-framework/utils'
 dotenv.config()
 
-const REPO_NAME = 'elsoul/skeet-discord-utils'
+const REPO_NAME = 'elsoul/skeet-cli'
 
-const run = async () => {
-  const token = process.env.DISCORD_TOKEN || ''
-  const channelId = process.env.DISCORD_CHANNEL_ID || ''
-  const channelIdJA = process.env.DISCORD_CHANNEL_ID_JA || ''
-  const laboSkeetChannelId = process.env.LABO_SKEET_CHANNEL_ID || ''
-  await discordChangeLog(token, REPO_NAME, [channelId])
-  await discordChangeLog(token, REPO_NAME, [channelIdJA], 'ja')
-  await discordChangeLog(token, REPO_NAME, [laboSkeetChannelId])
+const run = async (project: 'labo' | 'epics') => {
+  if (project === 'labo') {
+    console.log('labo')
+    const token = process.env.DISCORD_TOKEN_LABO || ''
+    const channelId = process.env.LABO_SKEET_CHANNEL_ID || ''
+    await discordChangeLog(token, REPO_NAME, [channelId])
+  } else if (project === 'epics') {
+    console.log('epics')
+    const token = process.env.DISCORD_TOKEN || ''
+    const channelId = process.env.DISCORD_CHANNEL_ID || ''
+    const channelIdJA = process.env.DISCORD_CHANNEL_ID_JA || ''
+    await discordChangeLog(token, REPO_NAME, [channelId])
+    await discordChangeLog(token, REPO_NAME, [channelIdJA], 'ja')
+  } else {
+    console.log('invalid project name')
+  }
 }
 
-run()
+const project = process.argv[2] as 'labo' | 'epics'
+run(project)
