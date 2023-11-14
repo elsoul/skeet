df9141be2419bf0e8f78eda32744dbac5ad90560
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-14 11:32:14 +0100

    update - skeet get https

--- src/cli/sub/list/listHttps.ts
+++ src/cli/sub/list/listHttps.ts
@@ -1,7 +1,9 @@
-import { FUNCTIONS_PATH, getHttpRoutings } from '@/lib'
+import { FUNCTIONS_PATH, getHttpRoutings, importConfig } from '@/lib'
+import { convertToKebabCase } from '@skeet-framework/utils'
 import * as cli from 'cli-table3'
 
 export const listHttps = async () => {
+  const { app } = importConfig()
   const https = await getHttpRoutings()
   const table = new cli.default({
     head: ['Function', 'Endpoint', 'ParamsPath'],
@@ -13,7 +15,11 @@ export const listHttps = async () => {
   const functionsEndpoints: string[][] = []
   for await (const file of https) {
     for await (const path of file.httpEndpoints) {
-      const endpoint = `${path}`
+      let endpoint = `https://${app.region}-${app.fbProjectId}.cloudfunctions.net/${path}`
+      if (app.hasLoadBalancer) {
+        const kebabcase = convertToKebabCase(path)
+        endpoint = `https://${app.lbDomain}/${file.functionName}/${kebabcase}`
+      }
       const paramsPath = `${FUNCTIONS_PATH}/${file.functionName}/src/types/http/${path}Params.ts`
       functionsEndpoints.push([file.functionName, endpoint, paramsPath])
     }
