28f0fe1d4052c770b0acb90a4db78b0972f351ae
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-15 18:01:54 +0100

    add - skeet add ghActions

--- src/cli/gen/genGithubActions.ts
+++ src/cli/gen/genGithubActions.ts
@@ -1,7 +1,13 @@
 import { execSync } from 'child_process'
+import { existsSync } from 'fs'
 
 export const genGithubActions = async () => {
   try {
+    const actionsPath = './github'
+    if (existsSync(actionsPath)) {
+      console.log('Github Actions already exists.')
+      return
+    }
     const cmd = ['mv', `./github`, `./.github`]
     execSync(cmd.join(' '))
   } catch (error) {
