274e0b6d3d1e8db9777ee446ad0522430b84781f
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-02-23 14:45:00 +0100

    update

--- packages/utils/build.ts
+++ packages/utils/build.ts
@@ -1,11 +1,6 @@
 import { build } from 'esbuild'
 import path from 'path'
 import { aliasPath } from 'esbuild-plugin-alias-path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-
-const __dirname = path.dirname(__filename)
 ;(async () => {
   await build({
     entryPoints: [path.resolve(__dirname, 'src/index.ts')],
