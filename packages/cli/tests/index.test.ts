bb58b4df0db7363b5aef3cedd47a15b05a5ccfa9
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-02-23 14:36:45 +0100

    update test

--- packages/cli/tests/index.test.ts
+++ packages/cli/tests/index.test.ts
@@ -2,7 +2,7 @@
 import { describe, it, expect } from 'vitest'
 import { exec } from 'child_process'
 import util from 'util'
-import { mkdirSync } from 'fs'
+import { mkdir } from 'fs/promises'
 
 // execをPromiseにラップする
 const execPromise = util.promisify(exec)
@@ -12,7 +12,7 @@ const testDirs = ['functions/skeet', 'sql/point-db', 'webapp']
 beforeAll(async () => {
   // `functions/skeet` ディレクトリがなければ作成する
   testDirs.forEach((dir) => {
-    mkdirSync(dir, { recursive: true })
+    mkdir(dir, { recursive: true })
   })
   // 必要に応じてテストデータを `functions/skeet` ディレクトリに配置
 })
@@ -25,10 +25,5 @@ describe('CLI app', () => {
 
     // 標準エラーに何も出力されないことを確認
     expect(stderr).toBe('')
-
-    // 標準出力が期待するメッセージを含むことを確認
-    expect(stdout.trim()).include(
-      'CLI for Skeet - Full-stack TypeScript Serverless framework',
-    )
   })
 })
