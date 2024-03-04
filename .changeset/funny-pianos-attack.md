8bc9672e62e5ae178fc568e1b7335d7246090fc7
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-03-04 19:45:02 +0100

    cloud task - migrate to esm

---
+++ .changeset/funny-pianos-attack.md
@@ -0,0 +1,5 @@
+---
+"@skeet-framework/cloud-task": minor
+---
+
+migrate to esm
---
+++ packages/cloud-task/build.ts
@@ -0,0 +1,31 @@
+// Import the necessary modules using ESM syntax
+import esbuild from 'esbuild'
+;(async () => {
+  console.log('Building...')
+  await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: true,
+    keepNames: true,
+    sourcemap: 'inline',
+    sourcesContent: true,
+    outdir: 'dist',
+    platform: 'node',
+    format: 'esm',
+    define: {
+      'process.env.NODE_ENV': `"production"`,
+    },
+    metafile: true,
+    external: [
+      'path',
+      '@skeet-framework/utils',
+      'fs',
+      'child_process',
+      'util',
+      'dotenv',
+      '@google-cloud/tasks',
+      'buffer',
+    ],
+  })
+  console.log('Build complete ⭐️')
+})()
