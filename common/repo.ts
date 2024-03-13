29b525024a4f359428654e75f9489dda7a972b27
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-03-13 13:52:22 +0100

    update base template

---
+++ common/repo.ts
@@ -0,0 +1,4 @@
+export const getTemplateRepo = (version = '1.0.1') => {
+  const BASE_TEMP = `https://registry.npmjs.org/@skeet-framework/base-template/-/base-template-${version}.tgz`
+  return BASE_TEMP
+}
