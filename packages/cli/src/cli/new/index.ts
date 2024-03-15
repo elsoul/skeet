0cd77a5bb83d3f4d80a8abc714c470116dabd1b5
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-03-15 16:59:46 +0100

    update validation for project name fix #227

--- packages/cli/src/cli/new/index.ts
+++ packages/cli/src/cli/new/index.ts
@@ -2,15 +2,26 @@ import { getTemplateRepo } from '@/config/repo'
 import { program } from '@/index'
 import { Logger } from '@/lib'
 import { updatePackageJsonName } from '@/lib/files/updatePackageJsonName'
+import chalk from 'chalk'
 import inquirer from 'inquirer'
-import { a } from 'vitest/dist/suite-UrZdHRff'
+
+const validateProjectID = (input: string) => {
+  // Google Cloud Project IDに適用する正規表現
+  const regex = /^[a-z][a-z0-9-]{4,28}[a-z0-9]$/
+  return (
+    regex.test(input) ||
+    chalk.yellow(
+      '⚠️ The project ID must start with a lowercase letter,\n contain only lowercase letters, numbers, and hyphens,\n must not end with a hyphen, and be 6-30 characters long.',
+    )
+  )
+}
 
 export const newCommands = async () => {
   program
     .command('new')
     .description('Create Skeet Framework App')
     .action(async () => {
-      const answer = await inquirer.prompt<{ name: string }>([
+      const answer = await inquirer.prompt([
         {
           type: 'input',
           name: 'name',
@@ -18,8 +29,10 @@ export const newCommands = async () => {
           default() {
             return 'skeet-app'
           },
+          validate: validateProjectID,
         },
       ])
+
       const result = await getTemplateRepo(answer.name)
       if (result) {
         await updatePackageJsonName(answer.name, answer.name + '/package.json')
