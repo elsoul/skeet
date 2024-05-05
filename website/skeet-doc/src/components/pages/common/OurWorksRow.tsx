c6fa6f39c307680c049956f27aaf5191c9fa19a8
KishiTheMechanic <s.kishi@elsoul.nl>
2024-05-05 05:13:58 +0200

    Show Skeet Solana Mainnet Validator

--- website/skeet-doc/src/components/pages/common/OurWorksRow.tsx
+++ website/skeet-doc/src/components/pages/common/OurWorksRow.tsx
@@ -1,41 +1,49 @@
 import Image from 'next/image'
 import GoogleCloudSkeet from '@/assets/img/work/GoogleCloudSkeet.png'
-import ValidatorList from '@/assets/img/work/ELSOULLABOSolanaMainnetValidator.jpg'
+import ValidatorListSkeet from '@/assets/img/work/SkeetSolanaMainnetValidator.jpg'
 import WBSO2023 from '@/assets/img/work/WBSO2023.png'
 import { useTranslation } from 'next-i18next'
 import GoogleCloudWeb3Startups from '@/assets/img/work/GoogleCloudWeb3Startups.png'
-import ENASE2024 from '@/assets/img/work/ENASE2024Accepted.jpg'
+import ENASE2024 from '@/assets/img/work/ELSOULLABOpresentedOnENASE2024.jpg'
+import { useMemo } from 'react'
 
 const works = [
   {
     title: 'OurWorksRow.GoogleCloudPartner.title',
-    imageUrl: GoogleCloudSkeet,
+    imageUrl: (isJapanese: boolean) => GoogleCloudSkeet,
+    imageLink: 'https://skeet.dev/',
     description: 'OurWorksRow.GoogleCloudPartner.description',
   },
   {
     title: 'OurWorksRow.WBSO.title',
-    imageUrl: WBSO2023,
+    imageLink: 'https://www.rvo.nl/subsidies-financiering/wbso',
+    imageUrl: (isJapanese: boolean) => WBSO2023,
     description: 'OurWorksRow.WBSO.description',
   },
   {
     title: 'OurWorksRow.SolanaValidator.title',
-    imageUrl: ValidatorList,
+    imageLink:
+      'https://www.validators.app/validators/LionBN8UQ69Amea77CurDSCnRDVGTMND9oyv74ZteE2?locale=en&network=mainnet',
+    imageUrl: (isJapanese: boolean) => ValidatorListSkeet,
     description: 'OurWorksRow.SolanaValidator.description',
   },
+
   {
     title: 'OurWorksRow.GoogleWeb3.title',
-    imageUrl: GoogleCloudWeb3Startups,
+    imageLink: 'https://epics.dev/',
+    imageUrl: (isJapanese: boolean) => GoogleCloudWeb3Startups,
     description: 'OurWorksRow.GoogleWeb3.description',
   },
   {
     title: 'OurWorksRow.ENASE2024.title',
-    imageUrl: ENASE2024,
+    imageLink: 'https://enase.scitevents.org/',
+    imageUrl: (isJapanese: boolean) => ENASE2024,
     description: 'OurWorksRow.ENASE2024.description',
   },
 ]
-
 export default function OurWorksRow() {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const isJapanese = useMemo(() => i18n.language === 'ja', [i18n])
   return (
     <>
       <div className="py-24 md:py-32 lg:py-40">
@@ -46,12 +54,21 @@ export default function OurWorksRow() {
           >
             {works.map((work) => (
               <li key={work.title}>
-                <Image
-                  className="aspect-[16/9] w-full rounded-md object-cover"
-                  src={work.imageUrl}
-                  alt={work.title}
-                  unoptimized
-                />
+                <a
+                  href={work.imageLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="hover:opacity-80"
+                >
+                  {' '}
+                  <Image
+                    className="aspect-[16/9] w-full rounded-md object-cover"
+                    src={work.imageUrl(isJapanese)}
+                    alt={work.title}
+                    unoptimized
+                  />
+                </a>
+
                 <h3 className="mt-6 text-lg font-bold leading-8 tracking-tight text-gray-900 dark:text-white">
                   {t(work.title)}
                 </h3>
