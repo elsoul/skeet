8b02aa46161a078bf1768c8fcf3a1b2d6d9739ce
KishiTheMechanic <s.kishi@elsoul.nl>
2024-10-05 03:57:56 +0200

    product row

--- website/skeet-dev/src/components/rows/ProductsSlideRow.tsx
+++ website/skeet-dev/src/components/rows/ProductsSlideRow.tsx
@@ -19,7 +19,7 @@ export default function ProductsSlideRow() {
         ]}
         className={cn(
           'h-[896px] w-full hover:cursor-grab active:cursor-grabbing sm:h-[1152px]',
-          'bg-gradient-to-b from-white via-zinc-200 to-white',
+          'bg-gradient-to-b from-white via-zinc-100 to-white',
           'dark:from-zinc-950 dark:via-indigo-950 dark:to-zinc-950',
         )}
       >
